7bea9f744a2a3e9fb4924df2e85093667ae56598
Jeff <cchsiao.jeff@gmail.com>
2025-06-14 17:29:18 +0800

    chart npm

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,90 @@
 import Image from "next/image";
 import { useRouter } from 'next/navigation'
 import Header from "@/components/Header/Header";
+// import { BarChart } from "@/lib/component/BarChart";
+import { BarChart } from "@tremor/react";
+
+const chartdata = [
+  {
+    date: "Jan 23",
+    SolarPanels: 2890,
+    Inverters: 2338,
+  },
+  {
+    date: "Feb 23",
+    SolarPanels: 2756,
+    Inverters: 2103,
+  },
+  {
+    date: "Mar 23",
+    SolarPanels: 3322,
+    Inverters: 2194,
+  },
+  {
+    date: "Apr 23",
+    SolarPanels: 3470,
+    Inverters: 2108,
+  },
+  {
+    date: "May 23",
+    SolarPanels: 3475,
+    Inverters: 1812,
+  },
+  {
+    date: "Jun 23",
+    SolarPanels: 3129,
+    Inverters: 1726,
+  },
+  {
+    date: "Jul 23",
+    SolarPanels: 3490,
+    Inverters: 1982,
+  },
+  {
+    date: "Aug 23",
+    SolarPanels: 2903,
+    Inverters: 2012,
+  },
+  {
+    date: "Sep 23",
+    SolarPanels: 2643,
+    Inverters: 2342,
+  },
+  {
+    date: "Oct 23",
+    SolarPanels: 2837,
+    Inverters: 2473,
+  },
+  {
+    date: "Nov 23",
+    SolarPanels: 2954,
+    Inverters: 3848,
+  },
+  {
+    date: "Dec 23",
+    SolarPanels: 3239,
+    Inverters: 3736,
+  },
+]
 
 export default function Home() {
   const router = useRouter()
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      jojo
-      ojojo
+    <div className="w-full flex flex-col dark">
+      <div className="text-L1-title flex items-center justify-start">
+        Statistic
+      </div>
+      <BarChart
+        className="h-80"
+        colors={["blue", "amber"]}
+        data={chartdata}
+        index="date"
+        categories={["SolarPanels", "Inverters"]}
+        valueFormatter={(number: number) =>
+          `$${Intl.NumberFormat("us").format(number).toString()}`
+        }
+        onValueChange={(v) => console.log(v)}
+        />
     </div>
   );
 }
